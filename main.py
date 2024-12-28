e8258e56edd1e1b5b00cb33da820b9b3816c2eb2
Luke100000 <jonas.peche@aon.at>
2024-12-28 17:34:40 +0100

    removed aioredis

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 
-import aioredis
 from fastapi import FastAPI
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
+from redis.asyncio.client import Redis
 
 from minecraft_recipe_renderer.api import setup
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncIterator[None]:
-    redis = aioredis.from_url("redis://localhost", decode_responses=False)
-    FastAPICache.init(RedisBackend(redis), prefix="minecraft-recipe-renderer")
+    FastAPICache.init(RedisBackend(Redis()), prefix="minecraft-recipe-renderer")
     yield
 
 
